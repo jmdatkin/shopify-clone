5e7541e2bf2085b0f7d74f7315898646b7af610f
Julian Atkin <jatkindev@gmail.com>
2022-03-24 12:37:45 -0400

    init repo

---
+++ index.mjs
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+
+
+app.listen(PORT, () => {
+    console.log("Server listening on *:"+PORT);
+});
