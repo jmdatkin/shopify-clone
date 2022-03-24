d988d05145bb630c1a3505d40084e0290fc1f578
Julian Atkin <jatkindev@gmail.com>
2022-03-24 14:33:55 -0400

    Changed some stuff, accidentally messed up some other stuff

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const express = require('express');
-// const knex = require('knex')({
-//     client: 'better-sqlite3',
-//     useNullAsDefault: true,
-//     connection: {
-//         filename: './database.sqlite'
-//     }
-// });
+const bodyParser = require('body-parser');
+const path = require('path');
 const knex = require('knex')(require('./knexfile').development);
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.get("/new", (req,res) => {
-    let name = req.query.name;
-    knex('pizza').insert({type: name})
-    .then(() =>res.status(200).send("Success!"))
-    .catch(err => {res.status(500).send("Error: "+err)});
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/", (req,res) => {
-    knex('pizza').select('*')
-    .then(data => res.status(200).send(data))
+app.use("/", express.static(path.resolve(__dirname, 'dist')));
+
+app.get("/api/product/:id", (req,res) => {
+    knex.select('*').from('products').where({
+        id: req.params.id
+    }).then(data => res.status(200).send(data))
     .catch(err => res.status(500).send("Error: "+err));
 });
+
+app.post("/api/product", (req,res) => {
+    let data = req.body;
+
+    console.log(data);
+
+    knex.table('products').insert(data)
+    .then(() => res.status(200).send("Success"))
+    .catch(err => res.status(500).send("Error: "+err));
+});
+
+app.delete("/api/product/:id", (req,res) => {
+    knex.table('products').where('id', req.params.id).del()
+    .then(() => res.status(200).send("Success"))
+    .catch(err => res.status(500).send("Error: "+err));
+});
+
+
 
 app.listen(PORT, async () => {
     console.log("Server listening on *:" + PORT);
