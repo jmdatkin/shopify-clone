a68e751c5f0ce0d1d3c091386cac848e62ddb4ff
Julian Atkin <jatkindev@gmail.com>
2022-03-24 14:38:02 -0400

    Fixed migrations error

---
+++ migrations/20220324183709_create_products_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('products', table => {
+        table.increments();
+        table.string('name');
+        table.string('desc');
+        table.decimal('price', 2);
+        table.binary('image');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('products');
+};
